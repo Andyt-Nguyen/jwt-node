f714dc14b2aa5fcb5241eebdecf7d26313c7d569
Andrew Ty Nguyen <andyt.nguyen123@gmail.com>
2018-04-13 08:44:50 -0700

    Protected route

--- index.js
+++ index.js
@@ -1,35 +1,74 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.post('/api/login', (req,res) => {
-	// auth user
-	const userId = { id: 1};
-	const token = jwt.sign({ user }, 'mysecretkey');
-	res.json({
-		token
+// app.get('/', (req, res) => {
+// 	res.json({
+// 		hello: 'hello'
+// 	})
+// });
+
+// This is an example of a non-protected route
+// app.post('/api/post', (req, res) => {
+// 	res.json({
+// 		message: 'Post created'
+// 	})
+// });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/',express.static(__dirname + '/public'));
+
+
+
+app.post('/api/login', (req, res) => {
+	// Mock user
+	const users = [
+		{userName: 'andy', password:'pass'},
+		{userName:'flipper', password:'pass'}
+	];
+	// jwt.sign(userInfo, secretkey, callback)
+	jwt.sign(req.body, 'secret_key',{expiresIn: '30s'} ,(err,token) => {
+		// console.log({token});
+		return res.status(200).json({token}).redirect('/api/login')
 	})
-})
+});
 
-
-app.get('/api/protected', (req, res) => {
-	jwt.verify(req.token, 'mysecretkey', (err, data) => {
-		if(err)  {
+app.use('/api/post', express.static(__dirname + '/secpublic'),verifyToken, (req, res) => {
+	console.log(req.token);
+	// The req.token now has the value of the token because of the
+	// verifyToken function;
+	jwt.verify(req.token, 'secret_key', (err, authData) => {
+		if(err) {
 			res.sendStatus(403);
 		} else {
-			res.json({text:'this is protected', data});
+			res.json({
+				message: 'Protected Route',
+				authData
+			});
 		}
-	})
-})
+	});
+});
 
-function ensureToken(req, res, next) {
-	const bearrerHeader = req.headers("authorization");
-	if(typeof bearrerHeader !== undefined) {
-		const bearer = bearrerHeader.split(' ');
-		const bearerToken = bearer[1];
+// Verifies users token
+function verifyToken(req, res, next) {
+	// Get auth header value
+	// Formatted token
+	// Authorization: Bearer <access_token>
+	const bearerHeader = req.headers['authorization'];
+	// Check of bearerHeader is undefined
+	if(typeof bearerHeader !== 'undefined') {
+		// In here we need to take out the access_token from the bearerHeader
+		const bearerToken = bearerHeader.split(' ')[1];
+		// Set the token
 		req.token = bearerToken;
+		// Set the next middleware
+		console.log(req.token);
 		next();
+
 	} else {
-		res.status(403)
+		res.sendStatus(403)
 	}
 }
+
+app.listen(5000, () => console.log(`Now listening on port 5000`));
